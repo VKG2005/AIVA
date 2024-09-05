813ed7458a83ba446f992b87a13af6f45b06603c
VK Goswami <143211818+VKG2005@users.noreply.github.com>
2024-09-05 22:01:34 +0530

    make a call for different desktop app using input command from user

--- main.py
+++ main.py
@@ -35,15 +35,27 @@ if __name__ == "__main__":
         print('Listening....')
         query = input_command()
 
-        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], ["google", "https://www.google.com"]]
+        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], ["google", "https://www.google.com"], ["spotify", "spotify:"] ,["whatsapp" ,"whatsapp:"]]
         for site in sites:
             if f"open {site[0]}" in query.lower():
                 say(f"Opening {site[0]} sir....")
                 webbrowser.open(site[1])
+                break
+
+        if site[0] == "spotify":
+            say("Opening Spotify...")
+            break
+
+        if site[0] == "whatsapp":
+            say("opening whatsapp........")
+            break
+
         if "open video" in query:
             videoPath = r"C:\Users\vines\New folder\WhatsApp Video 2024-08-19 at 19.47.56_5a11d9cd.mp4"
             os.startfile(videoPath)
+            break
+
         if "open pictures" in query:
             folderPath = "C:/Users/vines/OneDrive/Pictures"
-
             os.startfile(folderPath)
+            break
