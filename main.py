9ca7abc5983e9aae4739e89c464f8d92a887bf30
VK Goswami <143211818+VKG2005@users.noreply.github.com>
2024-09-05 21:37:38 +0530

    List of websites and their URLs and Iterate over the list of sites

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 import speech_recognition as sr
 import os
 import pyttsx3
+import pyaudio
+import webbrowser
+
 
 def say(text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
 
+
+def input_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:  # Fixed typo here
+        r.pause_threshold = 1
+
+        audio = r.listen(source)
+
+        try:
+            print ("recognizing....")
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said: {query}")
+        except Exception as e:
+            print("Sorry, I didn't catch that. Could you please say that again?")
+            return "none"
+        return query
+
+
 if __name__ == "__main__":
-    print('pycharm')
+    print('Pycharm')
     say("Hello, I am AIVA, your personal assistant")
+    while True:
+        print('Listening....')
+        query = input_command()
+
+        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], ["google", "https://www.google.com"]]
+        for site in sites:
+            if f"open {site[0]}" in query.lower():
+                say(f"Opening {site[0]} sir....")
+                webbrowser.open(site[1])
+
