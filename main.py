cf8c5be6f762217857c7631350b0f16c8eb1c0ac
VK Goswami <143211818+VKG2005@users.noreply.github.com>
2024-09-07 01:03:38 +0530

    make a function to perform specific task

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import speech_recognition as sr
+import datetime
 import os
-import pyttsx3
-import pyaudio
-import webbrowser
 import subprocess
+import webbrowser
+import pyttsx3
+import speech_recognition as sr
+import pyaudio
 
 
 def say(text):
+    print(f"Speaking: {text}")
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
@@ -14,7 +16,7 @@ def say(text):
 
 def input_command():
     r = sr.Recognizer()
-    with sr.Microphone() as source:  # Fixed typo here
+    with sr.Microphone() as source:
         r.pause_threshold = 1
 
         audio = r.listen(source)
@@ -22,44 +24,32 @@ def input_command():
         try:
             print ("recognizing....")
             query = r.recognize_google(audio, language="en-in")
+            # query = r.recognize_google(audio, language="hi-in")
             print(f"User said: {query}")
         except Exception as e:
             print("Sorry, I didn't catch that. Could you please say that again?")
             return "none"
         return query
 
+# def volume_adjust(query):
+#     if "volume increase" in query:
+#         subprocess.run([r"C:\Users\vines\PycharmProjects\AIVA\nircmd-x64\nircmd.exe", "changesysvolume", "10000"])
+#         say("volume increase")
+#     elif "decrease volume" in query:
+#         subprocess.run([r"C:\Users\vines\PycharmProjects\AIVA\nircmd-x64\nircmd.exe", "changesysvolume", "-10000"])
+#         say("volume decrease")
 
-if __name__ == "__main__":
-    print('Pycharm')
-    say("Hello, I am AIVA, your personal assistant")
+def main():
+    say("Hey boss,how are you?,I am your personal assistant")
     while True:
         print('Listening....')
         query = input_command()
-
-        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], ["google", "https://www.google.com"], ["spotify", "spotify:"] ,["whatsapp" ,"whatsapp:"]]
-        for site in sites:
-            if f"open {site[0]}" in query.lower():
-                say(f"Opening {site[0]} sir....")
-                webbrowser.open(site[1])
+        if query == 'none':
+            continue
 
 
-        if "open spotify" in query.lower():
-            say("Opening Spotify...")
-            subprocess.run(["start", "spotify:"], shell=True)
 
 
-        if "open whatsapp" in query.lower():
-            say("Opening WhatsApp...")
-            subprocess.run(["start", "whatsapp:"], shell=True)
-
-        if "open video" in query:
-            videoPath = r"C:\Users\vines\New folder\WhatsApp Video 2024-08-19 at 19.47.56_5a11d9cd.mp4"
-
-
-        if "open pictures" in query:
-            folderPath = "C:/Users/vines/OneDrive/Pictures"
-            os.startfile(folderPath)
-
-        if "stop the program" in query:
-            break
+if __name__ == "__main__":
+    main()
 
